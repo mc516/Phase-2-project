4ead4f403ba4eb9aa8b5115195eb93094aea6d30
Mickey <mickeyc11111@gmail.com>
2022-08-15 20:46:41 -0400

    Route,switch, and navlink added for home,about, and recipes page

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Home from "./Home"
 import About from "./About"
 import Recipes from "./Recipes";
+import NavBar from "./NavBar";
 import {Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route path="/about">
           <About />
