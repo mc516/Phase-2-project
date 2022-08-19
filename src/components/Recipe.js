bbfe806d7bb817b7136d97730c1708703acee227
Mickey <mickeyc11111@gmail.com>
2022-08-18 20:59:15 -0400

    added recipe card and recipe page components

---
+++ src/components/Recipe.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Recipe({recipeData}) {
+    const {name ,image, description, ingredients} = recipeData;
+    return (
+        <div className="recipe-card">
+            <div className="name">{name}</div>
+            <img src={image} alt={name}/> 
+            <div className="description">{description}</div>
+            <div className="ingredients">{ingredients}</div>
+        </div>  
+    )
+}
+
+export default Recipe
---
+++ src/components/RecipesPage.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react";
+import Recipe from "./Recipe";
+
+function Recipes() {
+    const [recipeList, setRecipeList] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/recipes')
+        .then(res => res.json())
+        .then(data => setRecipeList(data))
+    }, [])
+
+
+    return (
+        <div>
+            <h1>Recipes Page</h1>
+            {recipeList.map(recipe => {return <Recipe recipeData={recipe}/>} )}
+        </div>
+       
+    )
+}
+
+export default Recipes;
