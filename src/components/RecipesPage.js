e13fbf11eed063dcce8d24980319eaee27486556
Mickey <mickeyc11111@gmail.com>
2022-08-20 11:25:54 -0400

    search bar to filter recipe on recipes page

--- src/components/RecipesPage.js
+++ src/components/RecipesPage.js
@@ -3,6 +3,7 @@ import Recipe from "./Recipe";
 
 function Recipes() {
     const [recipeList, setRecipeList] = useState([])
+    const [search, setSearch] = useState("")
 
     useEffect(() => {
         fetch('http://localhost:3001/recipes')
@@ -10,12 +11,28 @@ function Recipes() {
         .then(data => setRecipeList(data))
     }, [])
 
-    const recipesToDisplay = recipeList.sort((a, b) => a.name.localeCompare(b.name))
+    
+    function handleSearch(e) {
+        console.log(e.target.value)
+        setSearch(e.target.value)
+    }
+
+    const recipesToDisplay = recipeList.filter(recipe => {
+        if(search !== "") {
+            return recipe.name.toLowerCase().includes(search.toLowerCase())
+        } else {
+            return true
+        }
+
+    })
     return (
         <div>          
             <h1>Recipes Page</h1>
+            <label htmlFor="search">Search: </label>
+            <input className="search-bar" type="text" onChange={handleSearch}></input>
+            <div></div>
             {
-                recipeList
+                recipesToDisplay
                 .sort((a, b) => a.name.localeCompare(b.name))
                 .map(recipe => {return <Recipe key={recipe.name} recipeData={recipe}/>})
             }      
