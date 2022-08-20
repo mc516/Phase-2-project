712efb0706b98301a1722550f9ffa31602be2745
Mickey <mickeyc11111@gmail.com>
2022-08-20 11:35:44 -0400

    delete button for each recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 function Recipe({recipeData}) {
-    const {name ,imageURL, description, ingredients} = recipeData;
+    const {name ,imageURL, description, ingredients, id} = recipeData;
+
+    function handleDeleteClick() {
+        console.log(id)
+    }
     return (
         <div className="recipe-card">
             <div className="name">{name}</div>
             <img className="image" src={imageURL} alt={name}/> 
             <div className="description">{description}</div>
             <div className="ingredients">{ingredients}</div>
-            
+            <button 
+                className="delete-btn"
+                onClick={handleDeleteClick}
+                >
+                Delete
+            </button>
          </div>  
     )
 }
